a8fc75c9c31b175c8a1ef94b8f404de3b97873f1
Joan Palacios <jopabel93@gmail.com>
2021-06-11 09:31:29 -0400

    Synchronization added (min(\tau) = 0)

--- src/pywarraychannels/channels.py
+++ src/pywarraychannels/channels.py
@@ -4,7 +4,7 @@ import pywarraychannels.filters
 
 ### Channel classes
 class Geometric():
-    def __init__(self, antenna_RX, antenna_TX, K = 128, f_c = 60e9, B = 1.760e9, filter = pywarraychannels.filters.RCFilter()):
+    def __init__(self, antenna_RX, antenna_TX, K = 128, f_c = 60e9, B = 1.760e9, filter = pywarraychannels.filters.RCFilter(), bool_sync = True):
         self.antenna_RX = antenna_RX
         self.antenna_TX = antenna_TX
         self.f_c = f_c
@@ -14,13 +14,17 @@ class Geometric():
         self.filter = filter
     def build(self, rays):
         channel = np.zeros([len(self.antenna_RX.antenna_elements), len(self.antenna_TX.antenna_elements), len(self.f_k_rel)], dtype = "complex128")
+        if bool_sync:
+            tau_min = np.min([ray[1] for ray in rays])
+        else:
+            tau_min = 0
         for phase, tau, power, doa_az, doa_el, dod_az, dod_el in rays:
             phase, doa_az, doa_el, dod_az, dod_el = np.radians(phase), np.radians(doa_az), np.radians(doa_el), np.radians(dod_az), np.radians(dod_el)   # Transform to radians
             doa = np.array([np.cos(doa_el)*np.cos(doa_az), np.cos(doa_el)*np.sin(doa_az), np.sin(doa_el)])                                              # Create direction vectors
             dod = np.array([np.cos(dod_el)*np.cos(dod_az), np.cos(dod_el)*np.sin(dod_az), np.sin(dod_el)])                                              # Create direction vectors
             scalar_doa = self.antenna_RX.scalar_dir(doa)
             scalar_dod = self.antenna_TX.scalar_dir(dod)
-            response_time = np.sqrt(len(self.f_k_rel))*self.filter.response(len(self.f_k_rel), tau*self.B)
+            response_time = np.sqrt(len(self.f_k_rel))*self.filter.response(len(self.f_k_rel), (tau-tau_min)*self.B)
             complex_gain = np.power(10, (power-30)/20)*np.exp(1j*phase)
             #channel += complex_gain*np.exp(1j*np.pi*(scalar_doa[:, np.newaxis, np.newaxis]-scalar_dod[np.newaxis, :, np.newaxis])*f_k_rel[np.newaxis, np.newaxis, :])*response_time[np.newaxis, np.newaxis, :]
             channel += ne.evaluate("cg*(cos(pi*(sdoa-sdod)*fk)+1j*sin(pi*(sdoa-sdod)*fk))*tr", global_dict = \
