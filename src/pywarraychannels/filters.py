0d4859a274e7cc2f6850ece933b1f3d9b01fe26c
Joan Palacios <jopabel93@gmail.com>
2021-06-16 09:54:25 -0400

    Vectorized RCF function

--- src/pywarraychannels/filters.py
+++ src/pywarraychannels/filters.py
@@ -1,24 +1,26 @@
 import numpy as np
 
+def __RCF_element__(rolloff_rc, t, M_rc):
+    denominator = 1 - np.power((2 * rolloff_rc * t / M_rc), 2)
+    if np.abs(denominator) < 1e-6:
+        return np.sinc(1/(2*rolloff_rc))*(np.pi/4)
+    else:
+        return np.sinc(t / M_rc) * \
+            np.cos(np.pi * rolloff_rc * t / M_rc) / \
+            denominator
+
+__RCF__ = np.vectorize(__RCF_element__)
+
 ### Filter classes
 class RCFilter():
-    def __init__(self, rolloff_rc = 0, M_rc = 1, early_samples = 0):
+    def __init__(self, rolloff_rc = 0, M_rc = 1, early_samples = 0, padding = 0):
         self.rolloff_rc = rolloff_rc
         self.M_rc = M_rc
         self.early_samples = early_samples
+        self.padding = padding
     def response(self, T, delay):
-        tt = np.arange(T)
-        if np.isscalar(delay) or len(np.array(delay).shape) == 0:
-            return np.sinc((tt + self.early_samples - delay) / self.M_rc) * \
-                np.cos(np.pi * self.rolloff_rc * (tt + self.early_samples - delay) / self.M_rc) / \
-                (1 - np.power((2 * self.rolloff_rc * (tt + self.early_samples - delay) / self.M_rc), 2))
-        else:
+        tt = np.arange(T + 2*self.padding)
+        if not np.isscalar(delay) and not len(np.array(delay).shape) == 0:
             tt = tt[:, np.newaxis]
             delay = np.array(delay)[np.newaxis, :]
-            denominator = 1 - np.power((2 * self.rolloff_rc * (tt + self.early_samples - delay) / self.M_rc), 2)
-            if np.abs(denominator) < 1e-6:
-                return np.sinc(1/(2*self.rolloff_rc))*(np.pi/4)
-            else:
-                return np.sinc((tt + self.early_samples - delay) / self.M_rc) * \
-                    np.cos(np.pi * self.rolloff_rc * (tt + self.early_samples - delay) / self.M_rc) / \
-                    denominator
+        return __RCF__(self.rolloff_rc, (tt + self.early_samples + self.padding - delay), self.M_rc)
