2623ed10e45ed62481e4b18b576cc2c69df8c263
Joan Palacios <jopabel93@gmail.com>
2021-08-20 15:26:31 -0400

    Option to flip RX and TX roles in Geometric rays

--- src/pywarraychannels/em.py
+++ src/pywarraychannels/em.py
@@ -2,7 +2,7 @@ import numpy as np
 
 ### Rays classes
 class Geometric():
-    def __init__(self, ray_info_or_phase, tau = None, power = None, doa_az = None, doa_el = None, dod_az = None, dod_el = None):
+    def __init__(self, ray_info_or_phase, tau = None, power = None, doa_az = None, doa_el = None, dod_az = None, dod_el = None, bool_flip_RXTX = False):
         if tau is None:
             ray_info = np.array(ray_info_or_phase)
             if len(ray_info.shape) == 1:
@@ -13,6 +13,12 @@ class Geometric():
                 ray_info = ray_info[np.newaxis, :]
             else:
                 ray_info = ray_info[np.newaxis, :].T
+        if bool_flip_RXTX:
+            ray_info[:, 3], ray_info[:, 4], ray_info[:, 5], ray_info[:, 6] = ray_info[:, 5], ray_info[:, 6], ray_info[:, 3], ray_info[:, 4]
+        self.ray_info = ray_info
+    def flip_RXTX(self):
+        ray_info = self.ray_info
+        ray_info[:, 3], ray_info[:, 4], ray_info[:, 5], ray_info[:, 6] = ray_info[:, 5], ray_info[:, 6], ray_info[:, 3], ray_info[:, 4]
         self.ray_info = ray_info
     def __iter__(self):
         return iter(self.ray_info)
